8a9293af1f9848ea8c2896eb76df8b9538446bd9
mwaqar <mwaqar@CalromLtd.onmicrosoft.com>
2024-11-11 20:14:50 +0500

    Docker compose setting

--- src/Web/EventEase.Web/Program.cs
+++ src/Web/EventEase.Web/Program.cs
@@ -6,17 +6,40 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSpaStaticFiles(configuration => configuration.RootPath = "ClientApp/dist");
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSpaStaticFiles();
+}
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseProxyToSpaDevelopmentServer(app.Configuration["NG_DEV_SERVER"]);
+    }
+    spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
+    {
+        OnPrepareResponse = context =>
+        {
+            if (context.File.Name == "index.html")
+            {
+                context.Context.Response.Headers.Add("Cache-Control", "no-cache, no-store");
+                context.Context.Response.Headers.Add("Max-Age", "0");
+            }
+        }
+    };
+});
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
